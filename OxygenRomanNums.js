db1335896d4a5a584c3e844f4b1517514c5aded5
Nick Wilson <Nick Wilson>
2016-01-12 13:58:01 -0600

    cleaned up final product

--- OxygenRomanNums.js
+++ OxygenRomanNums.js
@@ -35,11 +35,6 @@ function convertToRomanNumeral(userNumber){
 	console.log(romanNumber);
 }
 
-var decimals;
-decimals = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
-var romans;
-romans = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
-
 var userNumber;
 userNumber = checkUserNumber();
 convertToRomanNumeral(userNumber);
